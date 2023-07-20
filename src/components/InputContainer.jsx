b5f71047957aea09b86a8f3e37523b034b00715c
Mike Melchior <mike.melchior@outlook.com>
2023-07-20 15:31:57 -0400

    create container widget for input sections

---
+++ src/components/InputContainer.jsx
@@ -0,0 +1,9 @@
+function InputContainer( {children} ) {
+  return (
+    <div className='flex items-center md:w-[600px] bg-slate-50 rounded-lg'>  
+        {children}
+    </div>
+  )
+}
+
+export default InputContainer
